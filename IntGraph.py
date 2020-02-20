4ce7b21462719d623a16990f41b02d086f5009f1
DanielFuentes17083 <60523357+DanielFuentes17083@users.noreply.github.com>
2020-02-19 20:02:07 -0600

    Interfaz Grafica
    se implementa la parte de envio del contador de la interfaz grafica al puerto serial

---
+++ IntGraph.py
@@ -0,0 +1,45 @@
+from tkinter import *
+import serial
+import struct
+import time
+
+while(1): #Se inicializa la conexion con el puerto
+    while(1):
+        try:
+            numero = str(int(input(">  COM: ")))
+            port = "com" + numero
+            break
+        except:
+            print ("Enter a numeric value")
+    try:
+        data = serial.Serial(port, baudrate = 9600, timeout=1500)
+        break
+    except:
+        print("Unable to open port")
+
+
+gui = Tk(className='Comunicacion serial')
+# set window size
+gui.geometry("900x600")
+
+puerto = Label(gui, text = "COM:" + numero, font = ("Arial", 25))
+BT1 = Button(gui, text='Enviar valor')
+entry = Entry(gui, width=20)
+label1 = label(gui, text='Ingrese el valor del contador')
+
+puerto.pack()
+label1.place(x=50, y=25)
+BT1.place(x=100, y=100)
+entry.place(x=100, y=50)
+
+def ENVIO():
+    try:
+        contador = int(entry.get())
+        if contador < 255 and contador > 0:
+            data.write(struct.pack('>B',contador))
+        else:
+            messagebox.showerror('Error', 'el valor no puede ser mayor a 255 o menor que 0')
+    except:
+        messagebox.showerror('Error', 'Ingrese un numero entero')
+
+gui.mainloop()
