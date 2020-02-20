23cf578573aa538ce79d6aafc2e0b33d150e391b
DanielFuentes17083 <60523357+DanielFuentes17083@users.noreply.github.com>
2020-02-20 15:04:33 -0600

    100%
    Interfaz grafica funcional al 100%

--- GraphInt.py
+++ GraphInt.py
@@ -1,4 +1,5 @@
 from tkinter import * #https://likegeeks.com/es/ejemplos-de-la-gui-de-python/
+from tkinter import messagebox
 import serial 
 import struct
 import time
@@ -18,41 +19,49 @@ while(1): #Se inicializa la conexion con el puerto
         print("Unable to open port")
 
 
+
 gui = Tk(className='Comunicacion serial')
 # set window size
-gui.geometry("900x600")
-pot1 = StingVar()
-pot2 = StingVar()
+gui.geometry("550x250")
+pot1 = StringVar()
+pot2 = StringVar()
 pot1.set("0 V")
 pot2.set("0 V")
 
+
+
 puerto = Label(gui, text = "COM:" + numero, font = ("Arial", 25))
-BT1 = Button(gui, text='Enviar valor')
-entry = Entry(gui, width=20)
-label1 = label(gui, text='Ingrese el valor del contador')
-label2 = label(gui, text='S1:', font = ("Arial", 25))
-label3 = label(gui, text='S2:', font = ("Arial", 25))
-label4 = label(gui, textvariable=pot1, font = ("Arial", 25))
-label5 = label(gui, textvariable=pot2, font = ("Arial", 25))
+entry = Entry(gui, width=32)
+def ENVIO():
+    contador = entry.get()
+    if contador != "":
+        try:
+            contador = int(entry.get())
+            if contador <= 255 and contador >= 0:
+                data.write(struct.pack('>B',contador))
+            else:
+                messagebox.showerror(title='Error', message='el valor no puede ser mayor a 255 o menor que 0')
+        except:
+            messagebox.showerror(title='Error', message='Ingrese un numero entero')
+    else:
+        data.write(struct.pack('>B',0))
+BT1 = Button(gui, text='Enviar valor', command=ENVIO)
+label1 = Label(gui, text='Ingrese el valor del contador', font = ("Arial", 12))
+label2 = Label(gui, text='S1:', font = ("Arial", 25))
+label3 = Label(gui, text='S2:', font = ("Arial", 25))
+label4 = Label(gui, textvariable=pot1, font = ("Arial", 25))
+label5 = Label(gui, textvariable=pot2, font = ("Arial", 25))
 
 puerto.pack()
-label1.grid(column=5, row=3)
-BT1.grid(column=5, row=5)
-entry.grid(column=5, row=4)
-label2.grid(column=1, row=3)
-label3.grid(column=3, row=3)
-label4.grid(column=1, row=4)
-label5.grid(column=3, row=4)
+label1.place(x=300, y=70)
 
-def ENVIO():
-    try:
-        contador = int(entry.get())
-        if contador < 255 and contador > 0:
-            data.write(struct.pack('>B',contador))
-        else:
-            messagebox.showerror('Error', 'el valor no puede ser mayor a 255 o menor que 0')
-    except:
-        messagebox.showerror('Error', 'Ingrese un numero entero')
+label2.place(x=50, y=60)
+label3.place(x=200, y=60)
+label4.place(x=25, y=150)
+label5.place(x=180, y=150)
+entry.place(x=300, y=110)
+BT1.place(x=420, y=160)
+
 
 def getSerial():
     p1 = ord(data.read())
