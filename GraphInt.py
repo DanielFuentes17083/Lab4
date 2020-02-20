f08151a6da918002e123e9173f3c53f68f9b6e64
DanielFuentes17083 <60523357+DanielFuentes17083@users.noreply.github.com>
2020-02-20 15:09:45 -0600

    comentarios
    en el codigo py

--- GraphInt.py
+++ GraphInt.py
@@ -1,11 +1,11 @@
 from tkinter import * #https://likegeeks.com/es/ejemplos-de-la-gui-de-python/
-from tkinter import messagebox
-import serial 
-import struct
-import time
+from tkinter import messagebox #importa los mensajes de error
+import serial #libreria de comunicacion serial
+import struct #para convertir valores a formatos deseados
+import time #para refrescar valores
 
 while(1): #Se inicializa la conexion con el puerto
-    while(1):
+    while(1): #realiza las conecciones con el puerto serial
         try:
             numero = str(int(input(">  COM: ")))
             port = "com" + numero
@@ -20,7 +20,7 @@ while(1): #Se inicializa la conexion con el puerto
 
 
 
-gui = Tk(className='Comunicacion serial')
+gui = Tk(className='Comunicacion serial') #se define de forma general la interfaz grafica
 # set window size
 gui.geometry("550x250")
 pot1 = StringVar()
@@ -31,8 +31,8 @@ pot2.set("0 V")
 
 
 puerto = Label(gui, text = "COM:" + numero, font = ("Arial", 25))
-entry = Entry(gui, width=32)
-def ENVIO():
+entry = Entry(gui, width=32) #textbox
+def ENVIO(): #funcion que envia el valor de la textbox al puerto serial
     contador = entry.get()
     if contador != "":
         try:
@@ -45,13 +45,14 @@ def ENVIO():
             messagebox.showerror(title='Error', message='Ingrese un numero entero')
     else:
         data.write(struct.pack('>B',0))
-BT1 = Button(gui, text='Enviar valor', command=ENVIO)
+BT1 = Button(gui, text='Enviar valor', command=ENVIO) #boton
 label1 = Label(gui, text='Ingrese el valor del contador', font = ("Arial", 12))
 label2 = Label(gui, text='S1:', font = ("Arial", 25))
 label3 = Label(gui, text='S2:', font = ("Arial", 25))
-label4 = Label(gui, textvariable=pot1, font = ("Arial", 25))
-label5 = Label(gui, textvariable=pot2, font = ("Arial", 25))
+label4 = Label(gui, textvariable=pot1, font = ("Arial", 25)) #label con el valor del pot1
+label5 = Label(gui, textvariable=pot2, font = ("Arial", 25)) #label con el valor del pot2
 
+#posiciones de lo mostrado en el formulario
 puerto.pack()
 label1.place(x=300, y=70)
 
@@ -62,7 +63,7 @@ label5.place(x=180, y=150)
 entry.place(x=300, y=110)
 BT1.place(x=420, y=160)
 
-
+#funcion que recibe los valores y los convierte a 5 V del puerto serial
 def getSerial():
     p1 = ord(data.read())
     p2 = ord(data.read())
@@ -72,5 +73,6 @@ def getSerial():
     pot2.set(str(m2)+" V")
     gui.after(10,getSerial)
 
+#refresca los valores
 gui.after(10,getSerial)
 gui.mainloop()
